a8b308bb775aaae736b974413085c1d786280452
Brendon Jones <brendonj@waikato.ac.nz>
2013-01-21 16:22:57 +1300

    Move the result object into a separate file.

--- ampy/engine.py
+++ ampy/engine.py
@@ -12,6 +12,7 @@ import httplib
 import sys
 
 import sqlalchemy
+import ampy.result
 
 try:
     import pylibmc
@@ -153,7 +154,7 @@ class Connection(object):
             try:
                 if key in self.memcache:
                     #print "hit %s" % key
-                    return Result(self.memcache.get(key))
+                    return ampy.result.Result(self.memcache.get(key))
                 #else:
                 #    print "miss %s" % key
             except pylibmc.SomeErrors:
@@ -178,7 +179,7 @@ class Connection(object):
             except pylibmc.WriteError:
                 # Nothing useful we can do, carry on as if data was saved.
                 pass
-        return Result(data)
+        return ampy.result.Result(data)
 
     def get(self, src=None, dst=None, test=None, subtype=None, start=None, 
             end=None, binsize=60):
@@ -259,13 +260,13 @@ class Connection(object):
         # Just deal in test names, so create a list from the dict of {id:name}.
         if tests is not None:
             tests = tests.values()
-        return Result(tests)
+        return ampy.result.Result(tests)
     
     def _get_subtypes(self, src, dst, test, start, end):
         """ Fetches all test subtypes that are performed between src and dst """
         # FIXME Temporarily fetching using existing REST API, fetch from DB.
         subtypes = self._get_json("/".join([src, dst, test]), "subtypes")
-        return Result(subtypes)
+        return ampy.result.Result(subtypes)
 
     def _adjust_old_data(self, data):
         """ Strip the parent "data" that the old API uses """
@@ -289,73 +290,7 @@ class Connection(object):
         data = self._get_json("/".join(args), "dataset", binsize)
         if data is not None:
             data = map(self._adjust_old_data, data)
-        return Result(data)
-
-class Result(object):
-    """ Object to represent and facilitate fetching of results """
-
-    def __init__(self, data):
-        """ Create a new result object and associate a data source with it.
-            This datasource will be queried to provide result data as needed.
-
-            TODO: currently the datasource is simply a list, the functions to
-                  fetch data etc will need to change once a database is the 
-                  source
-        
-            Keyword arguments:
-            data -- the source of data for the results
-        """
-        # Catch empty lists used as markers
-        if data is None or len(data) == 0:
-            self.data = None
-        else:
-            self.data = data 
-        self.index = 0
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        """ Fetch the next item for the iterator """
-        if self.data is None:
-            raise StopIteration
-        if self.index < len(self.data):
-            self.index += 1
-            return self.data[self.index-1]
-        raise StopIteration 
-
-    def fetchone(self):
-        """ Fetch the next item in the result set, or None if all consumed """
-        try:
-            return self.next()
-        except StopIteration:
-            return None
-
-    def fetchall(self):
-        """ Fetch all remaining items in the result set """
-        if self.data is None:
-            return []
-        current = self.index
-        self.index = len(self.data)
-        return self.data[current:]
-
-    def fetchmany(self, count):
-        """ Fetch up to the specified number of results and return as a list """
-        if self.data is None:
-            return []
-        i = 0
-        result = []
-        while i < count and self.index < len(self.data):
-            result.append(self.data[self.index])
-            self.index += 1
-            i += 1
-        return result
-
-    def count(self):
-        """ Return the number of results this query produced """
-        if self.data is None:
-            return 0
-        return len(self.data)
+        return ampy.result.Result(data)
 
 
 # vim: set smartindent shiftwidth=4 tabstop=4 softtabstop=4 expandtab :
---
+++ ampy/result.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: ascii -*-
+
+"""
+Result object to allow easy fetching and use of results
+"""
+
+class Result(object):
+    """ Object to represent and facilitate fetching of results """
+
+    def __init__(self, data):
+        """ Create a new result object and associate a data source with it.
+            This datasource will be queried to provide result data as needed.
+
+            Keyword arguments:
+            data -- the source of data for the results
+        """
+        # Catch empty lists used as markers
+        if data is None or len(data) == 0:
+            self.data = None
+        else:
+            self.data = data 
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        """ Fetch the next item for the iterator """
+        if self.data is None:
+            raise StopIteration
+        if self.index < len(self.data):
+            self.index += 1
+            return self.data[self.index-1]
+        raise StopIteration 
+
+    def fetchone(self):
+        """ Fetch the next item in the result set, or None if all consumed """
+        try:
+            return self.next()
+        except StopIteration:
+            return None
+
+    def fetchall(self):
+        """ Fetch all remaining items in the result set """
+        if self.data is None:
+            return []
+        current = self.index
+        self.index = len(self.data)
+        return self.data[current:]
+
+    def fetchmany(self, count):
+        """ Fetch up to the specified number of results and return as a list """
+        if self.data is None:
+            return []
+        i = 0
+        result = []
+        while i < count and self.index < len(self.data):
+            result.append(self.data[self.index])
+            self.index += 1
+            i += 1
+        return result
+
+    def count(self):
+        """ Return the number of results this query produced """
+        if self.data is None:
+            return 0
+        return len(self.data)
+
+
+# vim: set smartindent shiftwidth=4 tabstop=4 softtabstop=4 expandtab :
