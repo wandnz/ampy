091efb0843843348397722879ad2267d51555e5e
Shane Alcock <salcock@waikato.ac.nz>
2013-08-27 16:14:43 +1200

    Supply suitable default argument to create_nntsc_engine for ampconfig

--- ampy/ampdb.py
+++ ampy/ampdb.py
@@ -13,7 +13,7 @@ def create_netevmon_engine(host, dbname, pwd, user):
     return ampy.netevmon.Connection(host, dbname, pwd, user)
 
 # Use this engine to get at core NNTSC information, e.g. the collection list
-def create_nntsc_engine(host, port, ampconfig):
+def create_nntsc_engine(host, port, ampconfig=None):
     return ampy.nntsc.Connection(host, port, ampconfig)
 
 # vim: set smartindent shiftwidth=4 tabstop=4 softtabstop=4 expandtab :
