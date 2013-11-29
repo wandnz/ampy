0ae5cecdfb3fc72e08d9cc332cda159078c1adb2
Shane Alcock <salcock@waikato.ac.nz>
2013-11-29 17:38:13 +1300

    Added support for amp icmp/traceroute views that are v4 or v6 only
    Also added functionality to create a group based on an event stream id.
    The stream id is nominal and describes *one* of the streams that make
    up the time series that the event was detected on. Using that stream, we
    now have to determine the group that it belongs to and create groups and
    views based on that.

--- ampy/nntsc.py
+++ ampy/nntsc.py
@@ -502,6 +502,17 @@ class Connection(object):
         # be used as selection options will differ from collection to
         # collection.
         return parser.get_selection_options(params)
+   
+    def event_to_group(self, name, streamid):
+        info = self.get_stream_info(name, streamid)
+        if info == {}:
+            return ""
+
+        parser = self._lookup_parser(name)
+        if parser == None:
+            return {}
+        
+        return parser.event_to_group(info)
     
     def stream_to_group(self, name, streamid):
         info = self.get_stream_info(name, streamid)
