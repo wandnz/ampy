ba023879fc657a77376f38df242a6f21a438d94f
Shane Alcock <salcock@waikato.ac.nz>
2016-07-05 17:04:08 +1200

    Bump version number to 2.4 in setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     description='Python library for interacting with NNTSC data.',
     packages = find_packages(),
     install_requires=requires,
-    version='2.2',
+    version='2.4',
     author='Shane Alcock, Brendon Jones',
     author_email='contact@wand.net.nz',
     url='http://www.wand.net.nz',
