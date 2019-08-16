95bb54c339289303166b784111c9b11c8abc85ad
Brendon Jones <brendonj@waikato.ac.nz>
2019-08-16 15:15:37 +1200

    Bump version number to 2.21 and update changelog.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     description='Python library for interacting with NNTSC data.',
     packages = find_packages(),
     install_requires=requires,
-    version='2.20',
+    version='2.21',
     author='Shane Alcock, Brendon Jones',
     author_email='contact@wand.net.nz',
     url='http://www.wand.net.nz',
